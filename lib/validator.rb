17f06148a2901c745b3f5a4f4cf7dc4aaa50e640
shaunnope <seanyap123@gmail.com>
2023-07-21 19:27:11 +0800

    feat: validate date format and value

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,5 +1,7 @@
 module Validator
     def valid_date?(date)
-        date =~ /^\d{4}-\d{2}-\d{2}$/
+        (date =~ /^\d{4}-\d{2}-\d{2}$/ || false) && Date.parse(date)
+    rescue Date::Error
+        false
     end
 end
